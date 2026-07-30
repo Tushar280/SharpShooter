ecad8e71920d75602da0780cd6ac31723a81b28f
Tushar280 <chaudarytushar280@gmail.com>
2026-07-30 16:11:33 +0530

    Add PlayerCapsule & Explo prefabs; spawn on death
    Add new prefabs: PlayerCapsule (Assets/Prefab/Models/PlayerCapsule.prefab) and explosion particle Explo (Assets/Prefab/Gun Effects/Explo.prefab). Update EnemyHealth to serialize a ParticleSystem field and Instantiate the explo at the enemy position with random rotation when health <= 0. Update Robot.prefab and Experiment.unity to reference and place the new prefabs/links accordingly.

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] int health = 30;
-
+    [SerializeField] ParticleSystem explo;
     public void TakeDamage(int damage)
     {
         health -= damage;
         if(health <= 0)
         {
+            Instantiate(explo,transform.position,Random.rotation);
             Destroy(gameObject);
         }
     }
