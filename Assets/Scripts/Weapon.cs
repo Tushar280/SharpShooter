39ce37ec266ffc5234b97168fe0b9bcfce0d04d5
Tushar280 <chaudarytushar280@gmail.com>
2026-07-30 15:45:08 +0530

    Gun Firing Update

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     [SerializeField] RaycastHit hit;
+    [SerializeField] ParticleSystem muzz;
 
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
+            muzz.Play();
             if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 100))
             {
                 Debug.Log(hit.transform.gameObject.name); 
