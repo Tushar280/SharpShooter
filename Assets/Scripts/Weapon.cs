7e24f3a9b3c9d7a1a51d4794fd253da0bf5a7914
Tushar280 <chaudarytushar280@gmail.com>
2026-07-30 16:00:01 +0530

    Add HitFX, Gun and Robot prefabs; spawn hit effect
    Add new prefabs and meta files for HitFX (particle effect), Gun and Robot models. Update Experiment.unity to reference those prefab instances. Modify Weapon.cs to expose a serialized ParticleSystem (hit1) and Instantiate the hit effect at raycast hit.point when firing.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,6 +4,7 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] RaycastHit hit;
     [SerializeField] ParticleSystem muzz;
+    [SerializeField] ParticleSystem hit1;
 
     private void Update()
     {
@@ -11,7 +12,8 @@ public class Weapon : MonoBehaviour
         {
             muzz.Play();
             if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 100))
-            {
+            {   
+                Instantiate(hit1,hit.point,Random.rotation);
                 Debug.Log(hit.transform.gameObject.name); 
                 EnemyHealth em = hit.transform.gameObject.GetComponent<EnemyHealth>();
 
