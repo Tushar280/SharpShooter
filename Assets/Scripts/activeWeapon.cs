37e43eff35212cb000721adddf449ffaef6dba8f
Tushar280 <chaudarytushar280@gmail.com>
2026-07-30 22:55:04 +0530

    Update activeWeapon.cs

--- Assets/Scripts/activeWeapon.cs
+++ Assets/Scripts/activeWeapon.cs
@@ -5,18 +5,29 @@ public class activeWeapon : MonoBehaviour
     Weapon weapon;
     [SerializeField] WeaponSO weaponSO;
 
+    float initTime;
+
     private void Start()
     {
         weapon = FindFirstObjectByType<Weapon>();
+        initTime = 0;
     }
 
     private void Update()
     {
+        initTime += Time.deltaTime;
+        
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
-            weaponSO.muzz.Play();
-            weapon.Shoot(weaponSO);
+            if(initTime >= weaponSO.firerate)
+            {
+                weaponSO.muzz.Play();
+                weapon.Shoot(weaponSO);
+                initTime = 0;
+            }
+            
         }
+        
     }
     
 }
