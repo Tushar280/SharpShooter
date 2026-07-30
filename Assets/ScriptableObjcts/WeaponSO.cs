4efa90676c3658b36fcafddaadd0e8812e5bfd75
Tushar280 <chaudarytushar280@gmail.com>
2026-07-30 22:36:47 +0530

    Add WeaponSO ScriptableObject and weapon assets
    Introduce WeaponSO ScriptableObject (CreateAssetMenu) with fields: damage (int) and firerate (float). Add three asset instances under Assets/ScriptableObjcts: Pistol (damage:2, firerate:3), Rifle (damage:4, firerate:5), and Sniper (damage:10, firerate:1). Includes generated .meta files for the new folder, script, and assets.

---
+++ Assets/ScriptableObjcts/WeaponSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponSO", menuName = "Scriptable Objects/WeaponSO")]
+public class WeaponSO : ScriptableObject
+{
+    public int damage;
+    public float firerate;
+    
+}
