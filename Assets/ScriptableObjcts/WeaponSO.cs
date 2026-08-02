912d1bf92fae5ce387dbb31a586a75ecc5a2bf30
Tushar280 <chaudarytushar280@gmail.com>
2026-08-02 14:24:50 +0530

    changes

--- Assets/ScriptableObjcts/WeaponSO.cs
+++ Assets/ScriptableObjcts/WeaponSO.cs
@@ -5,7 +5,9 @@ public class WeaponSO : ScriptableObject
 {
     public int damage;
     public float firerate;
+    public float magSize;
     public GameObject weaponPrefab;
     public bool canZoom;
     public float zoomFov;
+    
 }
