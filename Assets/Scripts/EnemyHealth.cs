c3235692ba92df28ad727ba9d455b67186b489fb
Tushar280 <chaudarytushar280@gmail.com>
2026-08-02 16:11:48 +0530

    Add robot self-destruct on player collision
    Implement kamikaze behavior for robots. When a robot collides with the player, it triggers self-destruction with particle effects. Changes include:
    
    - Added OnTriggerEnter to Robot.cs to detect player collision
    - Extracted destruction logic into SelfDestroy() method in EnemyHealth.cs
    - Added trigger collider to Robot prefab for collision detection
    - Disabled NavMeshAgent AutoBraking to improve collision behavior

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -4,14 +4,21 @@ public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] int health = 30;
     [SerializeField] ParticleSystem explo;
+
+
     public void TakeDamage(int damage)
     {
         health -= damage;
         if(health <= 0)
         {
-            ParticleSystem ExploVFX = Instantiate(explo,transform.position,Random.rotation);
-            Destroy(gameObject);
-            Destroy(ExploVFX.gameObject, 1f);
+            SelfDestroy();
         }
+    }
+
+    public void SelfDestroy()
+    {
+        ParticleSystem ExploVFX = Instantiate(explo,transform.position,Random.rotation);
+        Destroy(gameObject);
+        Destroy(ExploVFX.gameObject, 1f);
     }
 }
--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -4,15 +4,26 @@ public class Robot : MonoBehaviour
 {
     [SerializeField] GameObject target;
 
+    EnemyHealth em;
+
     UnityEngine.AI.NavMeshAgent agentRobo;
 
     private void Awake()
     {
+        em = GetComponent<EnemyHealth>();
         agentRobo = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
     private void Update()
     {
         agentRobo.SetDestination(target.transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            em.SelfDestroy();
+        }
     }
 }
