112507aaaa361efa5e818bcca6dcbf39ee596940
Tushar280 <chaudarytushar280@gmail.com>
2026-08-02 22:00:19 +0530

    Add PlayerHealth and AoE on enemy destroy
    Introduce PlayerHealth component (health, healthText, TakeDamage, SelfDestroy) and add area-of-effect damage to EnemyHealth.SelfDestroy using Physics.OverlapSphere(radius=3). Nearby PlayerHealth instances take 40 damage and EnemyHealth instances take 30. Adds PlayerHealth.cs and its .meta file.

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int health = 100;
+    [SerializeField] TMP_Text healthText;
+
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            SelfDestroy();
+        }
+    }
+
+    public void SelfDestroy()
+    {
+        ParticleSystem ExploVFX = Instantiate(explo,transform.position,Random.rotation);
+        Destroy(gameObject);
+        Destroy(ExploVFX.gameObject, 1f);
+    }
+
+    public void Update()
+    {
+
+    }
+}
