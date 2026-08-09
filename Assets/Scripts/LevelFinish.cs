d3cea990e9ebbd65a9b74a3f73bfc509525272a3
Tushar280 <chaudarytushar280@gmail.com>
2026-08-09 14:31:06 +0530

    Update LevelFinish.cs

--- Assets/Scripts/LevelFinish.cs
+++ Assets/Scripts/LevelFinish.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class LevelFinish : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnCollisionEnter(Collision collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Level Finished!");
+            // script
+        }
     }
 }
